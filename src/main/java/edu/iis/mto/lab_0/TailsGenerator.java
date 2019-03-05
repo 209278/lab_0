dc51670c594392b2a5e6d73b34178b1819d8c405
209278 <209278@edu.p.lodz.pl>
2019-03-05 17:51:08 +0100

    first passed

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -1,11 +1,20 @@
 package edu.iis.mto.lab_0;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class TailsGenerator {
 
     public List<String> tails(String value) {
+
+        if(value==null){
+            return Collections.emptyList();
+        }
+        
+        do{
+
+        }while(!value.equals(""))
         return new ArrayList<>(value.length());
     }
 
